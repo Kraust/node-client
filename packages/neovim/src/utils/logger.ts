0c520884e89e829bbc8abe44cb73b42012fedace
Kraust <secretdragoon@gmail.com>
2023-12-30 21:48:45 -0500

    Testing...

--- packages/neovim/src/utils/logger.ts
+++ packages/neovim/src/utils/logger.ts
@@ -1,5 +1,13 @@
-class Logger {
-
+export class Logger {
+  info(msg: string, ... $n: any[]) {
+    console.log(msg);
+  }
+  error(msg: string, ... $n: any[]) {
+    console.log(msg);
+  }
+  debug(msg: string, ... $n: any[]) {
+    console.log(msg);
+  }
 };
 
 function setupLogger(): Logger {
